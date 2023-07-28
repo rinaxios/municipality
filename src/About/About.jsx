a03cb1de640a930e2d92a68ea6a6f839154f037e
Rinat <rinaxios@gmail.com>
2023-07-28 21:30:54 +0600

    Created About component

---
+++ src/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './About.module.css'
+
+function About(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
+import About from "./About/About";
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <Main/>
+        <About/>
     </div>
   );
 }
