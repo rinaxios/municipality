f9a81596cc0fd6995b3c8c31d6efd74628709d1d
Rinat <rinaxios@gmail.com>
2023-08-12 20:28:14 +0600

    Created Deputies component

---
+++ src/About/Deputies/Deputies.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Deputies.module.css'
+
+function Deputies(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Deputies;
