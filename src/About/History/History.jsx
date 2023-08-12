04aa05a64b06007314c747683bf9d805d766e2af
Rinat <rinaxios@gmail.com>
2023-08-12 22:40:41 +0600

    Made button function show more

--- src/About/History/History.jsx
+++ src/About/History/History.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import s from './History.module.css'
 
+const contentStyles = {
+    WebkitLineClamp: 6,
+    WebkitBoxOrient: 'vertical',
+    overflow: 'hidden',
+    display: '-webkit-box',
+}
+
 function History(props) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const [showReadMoreBtn, setShowReadMoreBtn] = useState(false)
+
+    const ref = useRef(null)
+
+    useEffect(() => {
+        if (ref.current) {
+            setShowReadMoreBtn(
+                ref.current.scrollHeight !== ref.current.clientHeight
+            )
+        }
+    }, [])
+
     const options = {
         weekday: "short",
         year: "numeric",
@@ -13,7 +34,16 @@ function History(props) {
     return (
         <div className={s.history} id="history">
             <div className={s.title}>{props.title}</div>
-            <div className={s.content}>{props.content}</div>
+            <div className={s.content}>
+                <div style={isOpen ? null : contentStyles} ref={ref}>
+                    {props.content}
+                </div>
+                {showReadMoreBtn && (
+                    <span onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? '...скрыть' : '...показать ещё'}
+                    </span>
+                )}
+            </div>
             <div className={s.img}>
                 <img src={props.photo} alt=""/>
             </div>
