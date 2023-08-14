02a9bc620da41fd5bd6212f0156378a61d9194ca
Rinat <rinaxios@gmail.com>
2023-08-14 22:24:04 +0600

    Created Feedback component

---
+++ src/Contact/Feedback/Feedback.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Feedback.module.css'
+
+function Feedback(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Feedback;
